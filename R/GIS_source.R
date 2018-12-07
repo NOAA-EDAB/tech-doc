a6cad043e76c2666677f8c74f06d898b8b5cccda
seanhardison1 <seanhardison@gmail.com>
2018-12-07 11:37:13 -0500

    GIS source: Added thermal hab. plotting data

--- R/GIS_source.R
+++ R/GIS_source.R
@@ -4,13 +4,19 @@
 library(raster)
 library(ncdf4)
 library(rgdal)
+library(maps)
+library(mapdata)
+
 
 #Colors for SST anomalies
 colors <- colorRampPalette(c("darkblue",'blue', 'white', 'firebrick1',"firebrick4"))  
 color_levels=100   
 
 #GIS Directory
-gis.dir <- "gis"
+gis.dir <- here::here("gis")
+
+#Data directory
+data.dir <- here::here("data")
 
 #Projection
 map.crs <- CRS("+proj=longlat +lat_1=35 +lat_2=45 +lat_0=40 +lon_0=-77 +x_0=0
@@ -27,3 +33,17 @@ coast <- spTransform(coast,map.crs)
 
 #Extent for SST anomaly function
 e  <- extent(-77.2, -64.2, 35, 48)
+
+#Extent for thermal habitat
+th_extent <-extent(-77,-65,35,45) 
+
+#crop bathymetry
+th <- crop(bathy,th_extent)
+
+#projections
+projection(th) <- map.crs
+projection(bathy) <- map.crs
+
+#get strata files to clip thermal habitat projections
+strata <- readOGR(gis.dir,"strata", verbose = F)
+strata@proj4string <- map.crs
